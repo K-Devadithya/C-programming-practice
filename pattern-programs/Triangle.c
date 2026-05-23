7615cb1f669b3d3431077f0d941e63a4aaa8afe1
K-Devadithya <devaadithya57@gmail.com>
2026-05-23 21:57:46 +0530

    Create Triangle.c

---
+++ pattern-programs/Triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n,i,j;
+    printf("Enter the number of lines you want:");
+    scanf("%d",&n);
+for(i=1;i<=n;i++){        
+     for(j=1;j<=i;j++){   
+        printf("*");
+    }printf("\n");
+}
+getchar();
+getchar(); 
+ return 0;                   
+}
